83d4d54f493f4baabfad5550966879ea97e8541e
Rafael dos Santos <rafael@codash.com.br>
2022-09-10 01:24:14 -0300

    Trocando título da página de arquivo

--- post-types/class.sp4wp-cpt.php
+++ post-types/class.sp4wp-cpt.php
@@ -7,6 +7,7 @@ if ( ! class_exists( 'SP4WP_Post_Type' ) ){
         function __construct(){
 
             add_action( 'init', array( $this, 'create_post_type' ) );
+            add_filter( 'get_the_archive_title', array( $this, 'change_title_archive_page' ), 50 );
         }
 
         public function create_post_type(){
@@ -42,6 +43,15 @@ if ( ! class_exists( 'SP4WP_Post_Type' ) ){
                 'show_in_rest' => true, 
                 'menu_icon' => 'dashicons-images-alt2'
             ));
+        }
+
+        public function change_title_archive_page( $title ) {
+
+            if ( is_archive( 'sp4wp' ) ) {
+                $title = 'Portfólio';
+            }
+        
+            return $title;
         }
     }
 }
